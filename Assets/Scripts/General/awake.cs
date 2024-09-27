55744895946ed1deb76b767fd19c77f9443ce3cf
Naxa <ignaciaruizperez1@gmail.com>
2024-09-27 19:30:32 -0300

    dd

---
+++ Assets/Scripts/General/awake.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class awake : MonoBehaviour
+{
+    private void Awake()
+    {
+        Time.timeScale = 1;
+    }
+}
+
